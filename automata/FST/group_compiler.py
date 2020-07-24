a7374325812b170c3c5acf2ec1504905902d7727
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-07-24 19:50:12 +0100

    Extend the compiler wrappers

--- automata/FST/group_compiler.py
+++ automata/FST/group_compiler.py
@@ -6,6 +6,29 @@ def compile(automata_components):
     # to load into each table.
 
     # For now, compile just one to one other as a demo.
-    sc.compile(automata_components[0][0], automata_components[1][0])
+    for i in range(len(automata_components[0])):
+        sc.compile(automata_components[0][i], automata_components[1][i])
 
     return None, None
+
+
+# Estimate the amount of cross-compatability within
+# a suite of automata.
+def compile_statistics(connected_components):
+    algebras = [None] * len(connected_components)
+
+    print "Starting Compilation"
+    for i in range(len(connected_components)):
+        algebras[i] = sc.compute_depth_equation(connected_components[i], dump_output=True)
+
+    # Now, check for cross compatability:
+    cross_compilations = 0
+    for j in range(len(algebras)):
+        for i in range(len(algebras)):
+            if i != j:
+                result = sc.compare(algebras[i], algebras[j])
+                if result:
+                    print "Compiled ", algebras[i], " to ", algebras[j]
+                    cross_compilations += 1
+
+    print "Total cross compilations is ", cross_compilations
--- automata/FST/single_compiler.py
+++ automata/FST/single_compiler.py
@@ -1,5 +1,7 @@
 import automata as atma
 import sjss
+import algebra
+import time
 
 def compile(from_atma, to_atma):
     # Compare the shape of the autmata.  If the aren't roughly
@@ -7,36 +9,40 @@ def compile(from_atma, to_atma):
     depth_eqn_from = compute_depth_equation(from_atma)
     depth_eqn_to = compute_depth_equation(to_atma)
 
+    print "From Eqn:"
+    print str(depth_eqn_from)
+    print "To Eqn:"
+    print str(depth_eqn_to)
+
     # TODO -- Do something with those depth equations.
 
+def compare(eqn_from, eqn_to):
+    return algebra.leq(eqn_from, eqn_to)
 
-def compute_depth_equation(atma):
-    # Construct a lookup table from the node that inidicates
-    # the current depth.
 
-    # Convert the automata so we can apply the path algebra.
-    depth_eqn_automata, unification_constraints = atma.get_SJSS()
+def compute_depth_equation(atma, dump_output=False):
+    # Get the nodes, edges, starting and accepting states.
+    # Convert them to straight up integers to make everything
+    # else faster.
+    nodes = atma.node_ids_list()
+    edges = atma.edge_ids_list()
+    start = atma.fake_root.id
+    accepting_states = atma.reporting_states_list()
+    if dump_output:
+        print "Generating from :" + str(nodes)
+        print "and: " + str(edges)
+        print "with start: " + str(start)
+        print "and end: " + str(accepting_states)
+ 
+    start_time = time.time()
+    alg =  algebra.generate(nodes, edges, start, accepting_states)
+    end = time.time()
+    if dump_output:
+        print "Time taken is " + str(end - start_time)
+        if alg.size() > 1000:
+            print "Ommitting Algebra --- very long"
+            print "(Size was) " + str(alg.size())
+        else:
+            print "Generated Algebra: " + str(alg)
 
-    current_node = atma.root()
-    nodes_to_explore = []
-    algebra_terms = []
-
-    while len(nodes_to_explore) > 0:
-        # First, compute how many edges are in the current
-        # node, and if the current node is accepting.
-        # TODO
-
-        # Next, check for loops from this node.
-        # TODO
-
-        # If there are loops, compute the equations
-        # for those loops.
-        # TODO
-
-        # If there are branches, compute the equations
-        # for those branches
-        # TODO
-
-        # Progress onto the next node linearlly if there 
-        # is one.
-        # TODO
+    return alg
