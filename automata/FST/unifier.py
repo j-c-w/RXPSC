ef38c50b2665dc5ea82734fa571d2f8877564e46
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-19 18:51:55 +0100

    Fix crap heuristics that meant the unifier couldn't unify identical machines

--- automata/FST/unifier.py
+++ automata/FST/unifier.py
@@ -1,6 +1,7 @@
 # We need a value to indicate that an edges should not match
 # anything.
 NoMatchSymbol = None
+DEBUG_UNIFICATION = False
 
 class Unifier(object):
     def __init__(self):
@@ -26,23 +27,40 @@ class Unifier(object):
         disabled_states = []
 
         for i in range(len(self.from_edges)):
+            if DEBUG_UNIFICATION:
+                print "Trying to unify edges: "
+                print self.from_edges[i]
+                print self.to_edges[i]
+                print "Symbol sets are:"
+                print symbol_lookup_1[self.from_edges[i]]
+                print symbol_lookup_2[self.to_edges[i]]
             from_chars = symbol_lookup_1[self.from_edges[i]]
             if self.to_edges[i]:
                 to_chars = symbol_lookup_2[self.to_edges[i]]
             else:
+                if DEBUG_UNIFICATION:
+                    print "Found an edge with disabling required"
                 to_chars = None
 
             for from_char in from_chars:
                 if from_char in state_lookup:
-                    if state_lookup[from_char] not in to_chars:
+                    overlap_set = []
+                    for character in state_lookup[from_char]:
+                        if character in to_chars:
+                            overlap_set.append(character)
+
+                    if len(overlap_set) == 0:
+                        if DEBUG_UNIFICATION:
+                            print "Unification failed due to double-mapped state"
+                            print "(" + str(from_char) + ") already mapped to " + str(state_lookup[from_char]) + " when something in " + str(to_chars) + " is required"
                         return None
-                else:
-                    # Not already mapped:
-                    # COuld do this better, because this commits
-                    # the mapping to be to_chars[0] earlier rather
-                    # than later.
+                    else:
+                        # The overlap set is non-zero, but may
+                        # still be smaller than it was before.
+                        state_lookup[from_char] = overlap_set
+                else: # Fromchar not in state lookup.
                     if to_chars:
-                        state_lookup[from_char] = to_chars[0]
+                        state_lookup[from_char] = to_chars
                     else:
                         state_lookup[from_char] = None
                         disabled_states.append(from_char)
@@ -61,6 +79,8 @@ class Unifier(object):
                 non_matching = i
 
         if non_matching is None and len(disabled_states) != 0:
+            if DEBUG_UNIFICATION:
+                print "Unification failed due to required edge disabling that cannot be achieved"
             return None
         else:
             for disable in disabled_states:
@@ -69,7 +89,10 @@ class Unifier(object):
         # Translate everything else to itself.  This is an
         # relatively arbitrary decision I'm pretty sure.
         for i in range(0, 256):
-            if i not in state_lookup:
+            if i in state_lookup:
+                # Pick the first available option arbitrarily.
+                state_lookup[i] = state_lookup[i][0]
+            else:
                 state_lookup[i] = i
 
         return state_lookup
