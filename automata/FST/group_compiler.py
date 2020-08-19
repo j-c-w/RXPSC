6fd30390529230f95a68e3348a35c8870823ce60
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-19 18:51:42 +0100

    Add implementations of the group compilation methods

--- automata/FST/group_compiler.py
+++ automata/FST/group_compiler.py
@@ -1,15 +1,218 @@
 import single_compiler as sc
 
+DEBUG_COMPUTE_HARDWARE = False
+DEBUG_COMPUTE_COMPAT_MATRIX = False
+
+# This is a class that contains a set of accelerated
+# regular expressions.  We can use it to find which
+# expressions can be accelerated.
+group_id_counter = 0
+
+class CompiledObject(object):
+    def __init__(self, algebras, automatas):
+        global group_id_counter
+        assert len(algebras) == len(automatas)
+        self.algebras = algebras
+        self.automatas = automatas
+        self.group_id = group_id_counter
+        self.has_assignment = False
+        group_id_counter += 1
+
+    def set_assignment(assignment_indexes):
+        self.assignment_indexes = assignment_indexes
+        self.has_assignment = True
+
+    def add_pattern(algebra, automata):
+        assert self.has_assignment # Must already have assignment
+        # to add to hardware.
+
+        # Plan here is to find a hardware componenet
+        # that this can be added to.
+        # TODO
+        assert False
+
+# This is a class that contains the set of expressions
+# that /are/ being accelerated on the hardware.
+class HardwareAccelerators(object):
+    def __init__(self, groups):
+        # select the hardware we need, and compute
+        # the utilization.
+        self.hardware = compute_hardware_for(groups)
+        self.groups = groups
+
+# This is just a wrapper that stores the location and conversion
+# machine required for a particular regular expression.
+class CompilationIndex(object):
+    def __init__(self, i2, j2, conversion):
+        self.i = i2
+        self.j = j2
+        self.conversion_machine = conversion
+
+class AutomataContainer(object):
+    def __init__(self, automata, algebra):
+        self.automata = automata
+        self.algebra = algebra
+
+# Given a group, compute a 3D array representing the cross
+# compilability of that matrix.
+def compute_cross_compatibility_matrix_for(group):
+    results = [None] * len(group)
+    compilation_list = [None] * len(group)
+    # The compilation_list should be a 3D array that
+    # is the inverse the results array, i.e. it contains
+    # the list of all things that compile to a particular
+    # automata.
+    for i in range(len(group)):
+        contents = [None] * len(group[i])
+        for j in range(len(group[i])):
+            contents[j] = []
+        compilation_list[i] = contents
+
+    for i in range(len(group)):
+        row_results = [0] * len(group[i])
+        for j in range(len(group[i])):
+            # Now, compile everything that is /not/ in this
+            # group to this.
+            successful_compiles = []
+            source_automata = group[i][j]
+
+            for i2 in range(len(group)):
+                if i2 == i:
+                    # Don't want to count cross compilations
+                    # within the same group.
+                    continue
+
+                for j2 in range(len(group[i2])):
+                    target_automata = group[i2][j2]
+
+                    # We could make this faster by not generating
+                    # the conversion machine here --- it could
+                    # use just the depth equation equality
+                    # here and only generate the conversion
+                    # machine where needed (i.e. later when
+                    # things are actually assigned).
+                    if DEBUG_COMPUTE_COMPAT_MATRIX:
+                        print "Comparing ", str(source_automata.algebra)
+                        print " and, ", str(target_automata.algebra)
+                    conversion_machine = sc.compile_from_algebras(source_automata.algebra, source_automata.automata, target_automata.algebra, target_automata.automata)
+
+                    if conversion_machine:
+                        if DEBUG_COMPUTE_COMPAT_MATRIX:
+                            print "Successfully found a conversion between ", i2, j2
+                        successful_compiles.append(CompilationIndex(i2, j2, conversion_machine))
+                        compilation_list[i2][j2].append(CompilationIndex(i, j, conversion_machine))
+
+            row_results[j] = successful_compiles
+        results[i] = row_results
+
+    if DEBUG_COMPUTE_COMPAT_MATRIX:
+        print results
+    return results, compilation_list
+
+
+# Given a list of CompiledOjectGroup objects,
+# compute a set of regular expressions to
+# put on hardware, assignments for everything,
+# and conversion machines.
+def compute_hardware_assignments_for(groups):
+    compiles_from, compiles_to = compute_cross_compatibility_matrix_for(groups)
+    return assign_hardware(compiles_from, compiles_to)
+
+def assign_hardware(compiles_from, compiles_to):
+    # Greedy algorithm is to find the regex with the
+    # most coverage, and use that to 
+    # We could make this a lot faster with a heap or something.
+    
+    # Keep a list of indexes into the groups that indicate
+    # which ones we intend to put into hardware.
+    assigned_hardware = [None] * len(compiles_from)
+    for i in range(len(compiles_from)):
+        assigned_hardware[i] = [None] * len(compiles_from[i])
+
+    while True:
+        # Get the index of the most used compilation too.
+        max_compiles = 0
+        index = None
+        # Greedy step 1: pick the automata with the greatest
+        # coverage.
+        unassigned_hardware = False
+        for i in range(len(compiles_from)):
+            for j in range(len(compiles_from[i])):
+                if assigned_hardware[i][j] is not None:
+                    continue
+
+                unassigned_hardware = True
+                compiles = len(compiles_from[i][j])
+                if compiles >= max_compiles:
+                    max_compiles = compiles
+                    index = (i, j)
+
+        if DEBUG_COMPUTE_HARDWARE:
+            print "Next Interation:"
+            print "Found index with most overlap:"
+            print index
+
+        # All hardware is compiled to, return it.
+        if not unassigned_hardware:
+            return assigned_hardware
+
+        # Assign this peice of hardware to itself:
+        hardware_i, hardware_j = index
+        assigned_hardware[hardware_i][hardware_j] = CompilationIndex(hardware_i, hardware_j, None)
+
+        # Now, we (a) put that in hardware, we need to
+        # assign other automata that are part of different
+        # groups to it.  Greedy step 2: pick the automata
+        # with the fewest other options first.
+        for i in range(len(compiles_from)):
+            min_assigns = 100000000
+            min_assigns_index = None
+            min_assings_object = None
+            # find the min and min index.
+            for j in range(len(compiles_from[j])):
+                if assigned_hardware[i][j] is not None:
+                    # this has already been assigned a thing,
+                    # so doesnt need another one.
+                    continue
+                num_options = len(compiles_to[i][j])
+                # Check if this is a match:
+                is_match = False
+                match_obj = None
+                for option in compiles_to[i][j]:
+                    if option.i == index[0] and option.j == index[1]:
+                        if DEBUG_COMPUTE_HARDWARE:
+                            print "Match Found"
+                        is_match = True
+                        match_obj = option
+                if is_match and num_options < min_assigns:
+                    min_assigns = num_options
+                    min_assigns_index = j
+                    min_assigns_object = match_obj
+
+            # Now, create that particular assignment for group
+            # j.  Can only assign one per group.
+            # Could make this heuristic a bit better by deleting
+            # entries from this list.
+            if min_assigns_index is not None:
+                assigned_hardware[i][min_assigns_index] = CompilationIndex(index[0], index[1], min_assigns_object.conversion_machine)
+
+    return assigned_hardware
+
 def compile(automata_components):
-    # Takes a list of list of CCs, and returns a master
-    # automata that can run each group + a set of FSTs
-    # to load into each table.
+    # Takes a list of lists of CCs, and computes a
+    # set of CCs that should go in hardware, and a list
+    # that can be translated.
+    
+    # Generate the group.
+    groups = []
+    for cc_list in automata_components:
+        group = []
+        for cc in cc_list:
+            group.append(AutomataContainer(cc, sc.compute_depth_equation(cc)))
 
-    # For now, compile just one to one other as a demo.
-    for i in range(len(automata_components[0])):
-        sc.compile(automata_components[0][i], automata_components[1][i])
+        groups.append(group)
 
-    return None, None
+    return compute_hardware_assignments_for(groups)
 
 
 # Estimate the amount of cross-compatability within
