010302c3289827f32bca0f24e0fe2feffce7d241
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-26 00:20:25 +0100

    Add options for handling max recursive calls

--- automata/FST/options.py
+++ automata/FST/options.py
@@ -6,6 +6,9 @@ class Options(object):
         self.print_algebras = False
         self.profile = False
 
+        self.leq_iterations_file = None
+        self.leq_calls_threshold = 100000
+
 def create_from_args(args):
     opts = Options()
     opts.tail_approximation = args.tail_approximation
@@ -13,6 +16,8 @@ def create_from_args(args):
     opts.disabled_edges_approximation = args.disabled_edges_approximation
     opts.print_algebras = args.print_derived
     opts.profile = args.profile
+    opts.leq_iterations_file = args.leq_iterations_file
+    opts.leq_calls_threshold = args.leq_calls_threshold
 
     return opts
 
@@ -23,6 +28,8 @@ def add_to_parser(parser):
     parser.add_argument('--print-algebras', default=False, dest='print_derived',
             help='Print Derived Algebras', action='store_true')
     parser.add_argument('--profile', action='store_true', default=False, dest='profile', help='Run the profiler')
+    parser.add_argument('--leq-iterations-file', default=None, dest='leq_iterations_file', help='Dump file for the number of iterations of the LEQ operation')
+    parser.add_argument('--leq-calls-threshold', default=100000, dest='leq_calls_threshold', help='Number of recursive calls to make before giving up in the LEQ computation')
 
 EmptyOptions = Options()
 
