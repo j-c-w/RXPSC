5a50c5b5319fb59444ff30d31825cdde96cede1d
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-25 21:12:18 +0100

    Change interface for disabled edges to make it make more sense

--- automata/FST/unifier.py
+++ automata/FST/unifier.py
@@ -1,12 +1,19 @@
+import FST
 # We need a value to indicate that an edges should not match
 # anything.
 NoMatchSymbol = None
 DEBUG_UNIFICATION = False
+PRINT_UNIFICATION_FAILURE_REASONS = False
 
 class Unifier(object):
-    def __init__(self):
+    def __init__(self, algebra_from=None, algebra_to=None, cost=0):
         self.from_edges = []
         self.to_edges = []
+        self.disabled_edges = []
+        self.algebra_from = algebra_from
+        self.algebra_to = algebra_to
+        self.cost = cost
+        self.ununified_terms = []
 
     def add_edges(self, from_edges, to_edges):
         assert len(from_edges) == len(to_edges)
@@ -14,23 +21,46 @@ class Unifier(object):
         self.from_edges += from_edges
         self.to_edges += to_edges
 
+    def set_ununified_terms(self, terms):
+        assert self.ununified_terms == []
+        self.ununified_terms.appen(terms)
+
+    # Add some edges to make sure are never active.
+    def add_disabled_edges(self, edges):
+        self.disabled_edges += edges
+
+    def add_cost(self, cost):
+        self.cost += cost
+
+    def get_disabled_edges(self):
+        return self.disabled_edges
+
     def unify_with(self, other):
         self.from_edges += other.from_edges
         self.to_edges += other.to_edges
+        self.cost += other.cost
+        self.ununified_terms.append(other.ununified_terms)
 
+    # There may be some issues surrounding the naming convention
+    # of what is 'from' and what is 'to' in this function (and
+    # elsewhere tbh). Anyway, any issues with that should be aparent
+    # even if a pain.
     def unify_single_state(self, symbol_lookup_1, symbol_lookup_2):
         # This unifies into a single state.
         state_lookup = {}
         matching_symbol = {}
-        # List of characters that need to not trigger any edges
-        # in the accelerated state machine.
-        disabled_states = []
 
+        if DEBUG_UNIFICATION or PRINT_UNIFICATION_FAILURE_REASONS:
+            print "Starting new unification between "
+            print self.algebra_from
+            print self.algebra_to
         for i in range(len(self.from_edges)):
             if DEBUG_UNIFICATION:
                 print "Trying to unify edges: "
                 print self.from_edges[i]
                 print self.to_edges[i]
+                print symbol_lookup_1
+                print symbol_lookup_2
                 print "Symbol sets are:"
                 print symbol_lookup_1[self.from_edges[i]]
                 print symbol_lookup_2[self.to_edges[i]]
@@ -38,7 +68,7 @@ class Unifier(object):
             if self.to_edges[i]:
                 to_chars = symbol_lookup_2[self.to_edges[i]]
             else:
-                if DEBUG_UNIFICATION:
+                if DEBUG_UNIFICATION or PRINT_UNIFICATION_FAILURE_REASONS:
                     print "Found an edge with disabling required"
                 to_chars = None
 
@@ -50,7 +80,7 @@ class Unifier(object):
                             overlap_set.append(character)
 
                     if len(overlap_set) == 0:
-                        if DEBUG_UNIFICATION:
+                        if DEBUG_UNIFICATION or PRINT_UNIFICATION_FAILURE_REASONS:
                             print "Unification failed due to double-mapped state"
                             print "(" + str(from_char) + ") already mapped to " + str(state_lookup[from_char]) + " when something in " + str(to_chars) + " is required"
                         return None
@@ -62,8 +92,8 @@ class Unifier(object):
                     if to_chars:
                         state_lookup[from_char] = to_chars
                     else:
-                        state_lookup[from_char] = None
-                        disabled_states.append(from_char)
+                        assert False # I'm pretty sure that this
+                        # should never happen...
 
             if to_chars:
                 for char in to_chars:
@@ -78,12 +108,13 @@ class Unifier(object):
             if i not in matching_symbol:
                 non_matching = i
 
-        if non_matching is None and len(disabled_states) != 0:
+        disabled_edges = self.get_disabled_edges()
+        if non_matching is None and len(disabled_edges) != 0 and not options.disabled_edges_approximation:
             if DEBUG_UNIFICATION:
                 print "Unification failed due to required edge disabling that cannot be achieved"
             return None
-        else:
-            for disable in disabled_states:
+        elif non_matching is not None:
+            for disable in disabled_edges:
                 state_lookup[disable] = non_matching
 
         # Translate everything else to itself.  This is an
@@ -95,4 +126,6 @@ class Unifier(object):
             else:
                 state_lookup[i] = i
 
-        return state_lookup
+        if DEBUG_UNIFICATION or PRINT_UNIFICATION_FAILURE_REASONS:
+            print "Returning a real result"
+        return FST.SingleStateTranslator(state_lookup)
