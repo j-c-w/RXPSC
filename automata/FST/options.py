c443eaccadffbb63e549ada14f0a7162aaa102ea
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-27 21:07:15 +0100

    Add a number of debug options

--- automata/FST/options.py
+++ automata/FST/options.py
@@ -1,3 +1,5 @@
+import os
+
 class Options(object):
     def __init__(self):
         self.tail_approximation = False
@@ -9,6 +11,12 @@ class Options(object):
         self.leq_iterations_file = None
         self.leq_calls_threshold = 100000
 
+        self.group_size_distribution = None
+        self.print_file_info = False
+        self.dump_nodes_and_edges = None
+
+        self.graph_size_threshold = 2000
+
 def create_from_args(args):
     opts = Options()
     opts.tail_approximation = args.tail_approximation
@@ -19,6 +27,16 @@ def create_from_args(args):
     opts.leq_iterations_file = args.leq_iterations_file
     opts.leq_calls_threshold = args.leq_calls_threshold
 
+    opts.group_size_distribution = args.group_size_distribution
+    opts.print_file_info = args.print_file_info
+    opts.dump_nodes_and_edges = args.dump_nodes_and_edges
+
+    opts.graph_size_threshold = args.graph_size_threshold
+
+    if opts.dump_nodes_and_edges:
+        # Clear the file:
+        if os.path.exists(opts.dump_nodes_and_edges):
+            os.remove(opts.dump_nodes_and_edges)
     return opts
 
 def add_to_parser(parser):
@@ -30,6 +48,11 @@ def add_to_parser(parser):
     parser.add_argument('--profile', action='store_true', default=False, dest='profile', help='Run the profiler')
     parser.add_argument('--leq-iterations-file', default=None, dest='leq_iterations_file', help='Dump file for the number of iterations of the LEQ operation')
     parser.add_argument('--leq-calls-threshold', default=100000, dest='leq_calls_threshold', help='Number of recursive calls to make before giving up in the LEQ computation')
+    parser.add_argument('--print-file-info', default=False, dest='print_file_info', help='Print the file information', action='store_true')
+
+    parser.add_argument('--group-size-distribution', default=None, dest='group_size_distribution', help='Dump the group size distribution to this file')
+    parser.add_argument('--dump-nodes-and-edges', default=None, dest='dump_nodes_and_edges', help='Dump nodes and edges for each CC into a file')
+    parser.add_argument('--graph-size-threshold', default=2000, dest='graph_size_threshold', help="Exclude graphs larger than this value (deal with python recursion limit)")
 
 EmptyOptions = Options()
 
