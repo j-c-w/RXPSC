542192e53065da9b885f7117faa6abd0f95efd2c
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-08-29 11:23:43 +0100

    Add debug info about file groups

--- temp_scripts/FCCM/lut_based_8bit_groups.py
+++ temp_scripts/FCCM/lut_based_8bit_groups.py
@@ -8,15 +8,22 @@ import automata as atma
 import automata.FST.options as options
 import automata.HDL.hdl_generator as hd_gen
 import automata.FST.group_compiler as gc
+import automata.FST.compilation_statistics as compilation_statistics
 
 def process(file_groups, name, print_compression_stats=False, options=None):
+    file_groups = [os.path.join(file_groups, f) for f in os.listdir(file_groups) if os.path.isfile(os.path.join(file_groups, f))]
+    print "Extracting ", len(file_groups), " groups"
     # Output is: HDL file with the important automata
     # selected + table programming.
 
     automata_components = []
+    index = 0
     for file in file_groups:
-        if file.endswith('mnrl'):
-            print("ERROR: Entered a MNRL file, likely going to fail")
+        if not file.endswith('.anml'):
+            continue
+        if options.print_file_info:
+            print "Group Index: ", index, "comes from file", file
+            index += 1
 
         automatas = atma.parse_anml_file(file)
         # Need to remove ors to avoid 'special elements' that
@@ -47,11 +54,13 @@ def process(file_groups, name, print_compression_stats=False, options=None):
         print "COMPILATION STATISTICS: self compiles = ", self_compiles
         print "COMPILATION STATISTICS: Of those, ", equality, " were equal"
         print "COMPILATION STATISTICS: other compiles = ", other_compiles
+        print "COMPILATION STATISTICS: unifications = ", compilation_statistics.single_state_unification_success
+        print "COMPILATION STATISTICS: Of those,  ", compilation_statistics.exact_same_compilations, " were equal"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('name')
-    parser.add_argument('anml_file_groups', nargs='+')
+    parser.add_argument('anml_file_groups')
     parser.add_argument('--compression-stats', default=False, dest='compression_stats', action='store_true')
 
     options.add_to_parser(parser)
