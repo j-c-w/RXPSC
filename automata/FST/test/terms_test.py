0e650bbc910dba4e152a744eb9f9fd7637e12471
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-10-15 13:05:05 +0100

    Add some noramlization tests

--- automata/FST/test/terms_test.py
+++ automata/FST/test/terms_test.py
@@ -2,6 +2,14 @@ import unittest
 from automata.FST.terms import *
 
 class TermsTest(unittest.TestCase):
+    def test_normalize(self):
+        term = Branch([Const(2, [(5, 6), (6, 7)]), Sum([Const(2, [(4, 6), (6, 7)])])]).normalize()
+        self.assertEqual(str(term), "{1, 1} + 1")
+
+    def test_normalize_loop(self):
+        term = Branch([Sum([Const(2, [(5, 6), (6, 7)]), Product(Const(1, [(7, 7)]))]), Sum([Const(2, [(4, 6), (6, 7)]), Product(Const(1, [(7, 7)]))])]).normalize()
+        self.assertEqual(str(term), "{1, 1} + 1 + (1)*")
+
     def test_sizes(self):
         self.assertEqual(Sum([Branch([End(), End()])]).size(), 0)
 
