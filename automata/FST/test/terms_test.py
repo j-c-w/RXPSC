ab7d54e952517cca0f48fec1f37caf774ffbae49
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-11-06 01:15:36 +0000

    Add a get_first_node test

--- automata/FST/test/terms_test.py
+++ automata/FST/test/terms_test.py
@@ -20,12 +20,17 @@ class TermsTest(unittest.TestCase):
         self.assertEqual(str(Sum([Const(2, [(0, 1), (1, 2)]), End()]).split_last(1)[1]), "1 + e")
 
     def test_parse(self):
-        termsstring = "4 + {1 + (1)* + 1, 1} + {1 + (1)* + 1 + a + e, 1 + a + e}"
+        termsstring = "4 + {1, 1 + (1)* + 1} + {1 + a + e, 1 + (1)* + 1 + a + e}"
         self.assertEqual(str(parse_terms(termsstring)), str(termsstring))
 
     def test_accepting_nodes(self):
         t = Sum([Const(1, [(0, 1)]), Accept(), Product(Sum([Const(1, [(4, 5)]), Accept()]))])
         self.assertEqual(t.get_accepting_nodes(), set([1, 5]))
+
+    def test_first_node_branches(self):
+        t = Sum([Branch([Sum([Branch([Const(2, [(0, 1), (1, 5)]), Const(2, [(0, 4), (4, 5)])]), Const(1, [(5, 6)])]),
+            Const(1, [(0, 6)])]), Const(1, [(6, 8)])]).normalize()
+        self.assertEqual(t.get_first_node(), 0)
 
 if __name__ == "__main__":
     unittest.main()
